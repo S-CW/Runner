50e55e9031114717ea71164a4b028eba322cd3fe
Jeffrey <jaysoncheng999@gmail.com>
2023-08-31 19:01:04 +0800

    added background display

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400)) # window size
 pygame.display.set_caption("Runner") # window title
 clock = pygame.time.Clock()
+font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = font.render("My game", False, "Black")
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +17,9 @@ while True:
             pygame.quit()
             exit()
 
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300,50))
     
     pygame.display.update()
     clock.tick(60) # should not run faster than 60fps aka max fps
