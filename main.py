f93afd4c9e9cc411b9220a9341b99126517bfc02
Jeffrey <jaysoncheng999@gmail.com>
2023-09-06 00:08:34 +0800

    added player interaction

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ font = pygame.font.Font("font/Pixeltype.ttf", 50)
 # Stationary
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
-score_surf = font.render("My game", False, "Black").convert()
+score_surf = font.render("My game", False, (64,64,64)).convert()
 score_rect = score_surf.get_rect(center = (400, 50))
+game_active = True
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600,300))
 
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300)) # rectangle size of the image
+player_gravity = 0
 
 
 while True:
@@ -26,17 +28,45 @@ while True:
             pygame.quit()
             exit()
 
-    # backgeround
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(score_surf, score_rect)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+
+    if game_active:
+        # backgeround
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+        screen.blit(score_surf, score_rect)
 
 
-    snail_rect.left -= 4
-    if snail_rect.left < -100:
-        snail_rect.left = 800
-    screen.blit(snail_surf, snail_rect)
-    screen.blit(player_surf, player_rect)
+        snail_rect.left -= 4
+        if snail_rect.left < -100:
+            snail_rect.left = 800 
+        screen.blit(snail_surf, snail_rect)
+
+        # player
+        # ensure player is on the ground
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     pygame.display.update()
     clock.tick(60) # should not run faster than 60fps aka max fps
